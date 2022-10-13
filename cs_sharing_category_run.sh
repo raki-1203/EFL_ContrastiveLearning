03c280ff94375f9985bde0204daf5171437bae9e
raki-1203 <jfhdzzang@gmail.com>
2022-10-13 15:37:57 +0900

    ver5 데이터 학습 shell file

--- cs_sharing_category_run.sh
+++ cs_sharing_category_run.sh
@@ -5,9 +5,9 @@
 
 for lr in 0.00001 0.00005
 do
-for cl_weight in 0.9 0.6 0.3 0.1
+for cl_weight in 0.6 0.3 0.1
 do
-for temperature in 0.05 0.10 0.15 0.20 0.25 0.5
+for temperature in 0.25 0.5
 do
 python3 train.py \
   --is_train \
@@ -16,11 +16,11 @@ python3 train.py \
   --write_summary \
   --device 1 \
   --method efl_scl \
-  --output_path ./model/saved_model/all_category_model_7_3 \
+  --output_path ./model/saved_model/three_category_model_ver5 \
   --model_name_or_path ./model/checkpoint-2000000 \
   --vocab_path ./tokenizer/version_1.9 \
-  --path_to_train_data ./data/cs_sharing/train_ver2 \
-  --path_to_valid_data ./data/cs_sharing/valid_ver2 \
+  --path_to_train_data ./data/cs_sharing/train_ver5 \
+  --path_to_valid_data ./data/cs_sharing/valid_ver5 \
   --max_len 256 \
   --batch_size 32 \
   --lr ${lr} \
@@ -29,7 +29,7 @@ python3 train.py \
   --epochs 10 \
   --pooler_option cls \
   --eval_steps 100 \
-  --tensorboard_dir tensorboard_logs/all_category_model_7_3 \
+  --tensorboard_dir tensorboard_logs/three_category_model_ver5 \
   --warmup_ratio 0.05 \
   --temperature ${temperature} \
   --trial 0 \
@@ -37,6 +37,41 @@ python3 train.py \
 done
 done
 done
+
+#for lr in 0.00001 0.00005
+#do
+#for cl_weight in 0.6 0.3 0.1
+#do
+#for temperature in 0.25 0.5
+#do
+#python3 train.py \
+#  --is_train \
+#  --task category \
+#  --use_amp \
+#  --write_summary \
+#  --device 1 \
+#  --method efl_scl \
+#  --output_path ./model/saved_model/all_category_model_ver4 \
+#  --model_name_or_path ./model/checkpoint-2000000 \
+#  --vocab_path ./tokenizer/version_1.9 \
+#  --path_to_train_data ./data/cs_sharing/train_ver4 \
+#  --path_to_valid_data ./data/cs_sharing/valid_ver4 \
+#  --max_len 256 \
+#  --batch_size 32 \
+#  --lr ${lr} \
+#  --weight_decay 0.1 \
+#  --cl_weight ${cl_weight} \
+#  --epochs 10 \
+#  --pooler_option cls \
+#  --eval_steps 100 \
+#  --tensorboard_dir tensorboard_logs/all_category_model_ver4 \
+#  --warmup_ratio 0.05 \
+#  --temperature ${temperature} \
+#  --trial 0 \
+#  --seed 42
+#done
+#done
+#done
 
 
 #for lr in 0.00001 0.00005
@@ -52,7 +87,43 @@ done
 #  --write_summary \
 #  --device 1 \
 #  --method efl_scl \
-#  --output_path ./model/saved_model/all_category_model_9_1 \
+#  --output_path ./model/saved_model/all_category_model_ver2 \
+#  --model_name_or_path ./model/checkpoint-2000000 \
+#  --vocab_path ./tokenizer/version_1.9 \
+#  --path_to_train_data ./data/cs_sharing/train_ver2 \
+#  --path_to_valid_data ./data/cs_sharing/valid_ver2 \
+#  --max_len 256 \
+#  --batch_size 32 \
+#  --lr ${lr} \
+#  --weight_decay 0.1 \
+#  --cl_weight ${cl_weight} \
+#  --epochs 10 \
+#  --pooler_option cls \
+#  --eval_steps 100 \
+#  --tensorboard_dir tensorboard_logs/all_category_model_ver2 \
+#  --warmup_ratio 0.05 \
+#  --temperature ${temperature} \
+#  --trial 0 \
+#  --seed 42
+#done
+#done
+#done
+
+
+#for lr in 0.00001 0.00005
+#do
+#for cl_weight in 0.9 0.6 0.3 0.1
+#do
+#for temperature in 0.05 0.10 0.15 0.20 0.25 0.5
+#do
+#python3 train.py \
+#  --is_train \
+#  --task category \
+#  --use_amp \
+#  --write_summary \
+#  --device 1 \
+#  --method efl_scl \
+#  --output_path ./model/saved_model/all_category_model_ver3 \
 #  --model_name_or_path ./model/checkpoint-2000000 \
 #  --vocab_path ./tokenizer/version_1.9 \
 #  --path_to_train_data ./data/cs_sharing/train_ver3 \
@@ -65,7 +136,7 @@ done
 #  --epochs 10 \
 #  --pooler_option cls \
 #  --eval_steps 100 \
-#  --tensorboard_dir tensorboard_logs/all_category_model_9_1 \
+#  --tensorboard_dir tensorboard_logs/all_category_model_ver3 \
 #  --warmup_ratio 0.05 \
 #  --temperature ${temperature} \
 #  --trial 0 \
